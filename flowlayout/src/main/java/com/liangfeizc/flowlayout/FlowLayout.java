c3b1bd47c579653a16a90defca6d383a48bbb6d4
feelang <liangfeizc@163.com>
2015-02-11 14:04:40 +0800

    baby born

---
+++ flowlayout/src/main/java/com/liangfeizc/flowlayout/FlowLayout.java
@@ -0,0 +1,122 @@
+package com.liangfeizc.flowlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liangfei on 2/10/15.
+ */
+
+/**
+ * Inspired by
+ * http://hzqtc.github.io/2013/12/android-custom-layout-flowlayout.html
+ */
+public class FlowLayout extends ViewGroup {
+    private int mVerticalSpacing;
+    private int mHorizontalSpacing;
+
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FlowLayout);
+        try {
+            mHorizontalSpacing = a.getDimensionPixelSize(R.styleable.FlowLayout_horizontal_spacing, 5);
+            mVerticalSpacing = a.getDimensionPixelSize(R.styleable.FlowLayout_vertical_spacing, 5);
+        } finally {
+            a.recycle();
+        }
+    }
+
+
+    public void setHorizontalSpacing(int pixelSize) {
+        mHorizontalSpacing = pixelSize;
+    }
+
+    public void setVerticalSpacing(int pixelSize) {
+        mVerticalSpacing = pixelSize;
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int myWidth = resolveSize(0, widthMeasureSpec);
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int childLeft = paddingLeft;
+        int childTop = paddingTop;
+
+        int lineHeight = 0;
+
+        // Measure each child and put the child to the right of previous child
+        // if there's enough room for it, otherwise, wrap the line and put the child to next line.
+        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
+            View childView = getChildAt(i);
+            LayoutParams childLayoutParams = childView.getLayoutParams();
+            childView.measure(
+                    getChildMeasureSpec(widthMeasureSpec, paddingLeft + paddingRight, childLayoutParams.width),
+                    getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom, childLayoutParams.height));
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            lineHeight = Math.max(childHeight, lineHeight);
+
+            if (childLeft + childWidth + paddingRight > myWidth) {
+                childLeft = paddingLeft;
+                childTop += mVerticalSpacing + lineHeight;
+                lineHeight = childHeight;
+            } else {
+                childLeft += childWidth + mHorizontalSpacing;
+            }
+        }
+
+        int wantedHeight = childTop + lineHeight + paddingBottom;
+        setMeasuredDimension(myWidth, resolveSize(wantedHeight, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int myWidth = r - l;
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+
+        int childLeft = paddingLeft;
+        int childTop = paddingTop;
+
+        int lineHeight = 0;
+
+        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
+            View childView = getChildAt(i);
+
+            if (childView.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            lineHeight = Math.max(childHeight, lineHeight);
+
+            if (childLeft + childWidth + paddingRight > myWidth) {
+                childLeft = paddingLeft;
+                childTop += mVerticalSpacing + lineHeight;
+                lineHeight = childHeight;
+            }
+
+            childView.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+            childLeft += childWidth + mHorizontalSpacing;
+        }
+    }
+}
